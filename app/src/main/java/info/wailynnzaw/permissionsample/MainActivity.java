946a92ee9a4b0feff8e61678181e586367d02795
Wai Lynn Zaw <wailynnzaw.lamz@gmail.com>
2018-08-05 11:20:46 +0630

    Change phone Number

--- app/src/main/java/info/wailynnzaw/permissionsample/MainActivity.java
+++ app/src/main/java/info/wailynnzaw/permissionsample/MainActivity.java
@@ -1,8 +1,13 @@
 package info.wailynnzaw.permissionsample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -24,44 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
-  private void makeCall(){
-    String url = "tel:"+ "095500619";
+  
+  private void makeCall() {
+    String url = "tel:" + ("095500619");
     Intent it = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        requestPermissions(new String[]{Manifest.permission.CALL_PHONE},
+                100);
+      }
+      return;
+    }
     startActivity(it);
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  private void makeCall() {
-//    String url = "tel:" + ("095500619");
-//    Intent it = new Intent(Intent.ACTION_CALL, Uri.parse(url));
-//    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//        requestPermissions(new String[]{Manifest.permission.CALL_PHONE},
-//                100);
-//      }
-//      return;
-//    }
-//    startActivity(it);
-//  }
-//
-//  @Override
-//  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//    if (requestCode == 100
-//            && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//      makeCall();
-//    }
-//  }
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == 100
+            && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      makeCall();
+    }
+  }
 }
